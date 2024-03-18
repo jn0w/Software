45eea09e940d0c30125425ec324c7f472d37412c
jn0w <B00153676@mytudublin.ie>
2024-03-18 13:57:26 +0000

    removed unecessary sql files

--- FINAL_BiedronkaSLC/Biedronka/data/admin.sql
+++
@@ -1,4 +0,0 @@
-USE grocery_shop;
-
-INSERT INTO users (firstname, lastname, email, password, role, address, contact_number) VALUES 
-('Admin', 'User', 'admin@gmail.com', '1234567890', 'admin', 'admin', 'admin');
